d6530de30efc4c7c9c2031ebc80d0c7224b32431
tpst <virtuosophantom@gmail.com>
2022-07-20 01:08:55 +0530

    created base files

---
+++ my-app/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Navbar from './navbar/Navbar';
+function App() {
+  return (
+    <div className="App">
+      <Navbar/>
+      <header className="App-header">
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ my-app/src/navbar/Navbar.js
@@ -0,0 +1,10 @@
+const Navbar = () => {
+    return ( 
+
+        <nav class="navbar">
+            <h1>The palash blog</h1>
+        </nav>
+     );
+}
+ 
+export default Navbar;
